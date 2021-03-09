9a4b9ffb8fce927fbe17f4a33a57e4164849fb4f
Alena Trushnikova <alena.yu.t@gmail.com>
2021-03-09 11:56:07 -0800

    errors handling

--- app/controllers/auth_controller.rb
+++ app/controllers/auth_controller.rb
@@ -10,13 +10,12 @@ class AuthController < ApplicationController
 
       render json: {id: UserSerializer.new(user), token: token}
     else
-      render json: {error: 'Invalid email or password, please Sign Up'}, status: 401
+      render json: {error: 'Please enter the valid email address and password combination'}, status: :unauthorized
     end
   end
 
   def show
     user = User.find(params[:id])
-
     render json: UserSerializer.new(user)
   end
 
